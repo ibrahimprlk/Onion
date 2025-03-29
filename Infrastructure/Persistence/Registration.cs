9eb16d4f284bd6c601c2fa6dac4c78a9bb0ff369
İbrahim Parlak <ibrahimparlak3694@gmail.com>
2025-03-29 16:46:00 +0300

    bug fixed

---
+++ Infrastructure/Persistence/Registration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public static class Registration
+    {
+        public static void AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(opt =>
+            opt.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        }
+    }
+}
--- Presentation/WebAPI/Program.cs
+++ Presentation/WebAPI/Program.cs
@@ -1,3 +1,4 @@
+using Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,8 +13,8 @@ var env =builder.Environment;
 builder.Configuration
     .SetBasePath(env.ContentRootPath)
     .AddJsonFile("appsettings.json",optional:false)
-    .AddJsonFile($"appsettings.{env.EnvironmentName}.json",optional:true); 
-
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json",optional:true);
+builder.Services.AddPersistence(builder.Configuration);
 
 var app = builder.Build();
 
